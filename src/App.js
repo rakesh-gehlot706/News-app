93e17c8115aae834e450f83b10f79e8a135f6400
rakesh-gehlot <gehlotrakesh706@gmail.com>
2023-10-05 16:52:23 +0530

    feature: navigation bar

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default class App extends Component {
     return (
       <div>
        <NavBar/>
-       <News/>
+       <News pageSize={6} country="in" category="Science"/>
       
       </div>
     )
